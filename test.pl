01936d176fbbc090e9b67ddf39bd5859cf99971a
Slaven Rezic <slaven@rezic.de>
1998-05-24 18:28:29 +0000

    $tmp instead of /tmp -format test

--- test.pl
+++ test.pl
@@ -2,7 +2,7 @@
 # -*- perl -*-
 
 #
-# $Id: test.pl,v 1.1 1998/04/15 03:41:26 eserte Exp $
+# $Id: test.pl,v 1.2 1998/05/24 18:28:29 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 1998 Slaven Rezic. All rights reserved.
@@ -13,7 +13,7 @@
 # WWW:  http://user.cs.tu-berlin.de/~eserte/
 #
 
-BEGIN { print "1..5\n" }
+BEGIN { $last = 8; print "1..$last\n" }
 
 use Tk;
 use Tk::TIFF;
@@ -21,25 +21,43 @@ use File::Compare;
 
 $ok = 1;
 print "ok " . $ok++ . "\n";
+$tmp = "/tmp";
 
 $top = new MainWindow;
 
-foreach (qw(test-2channel.tif test-lzw.tif test-none.tif test-packbits.tif)) {
+foreach (qw(test-none.tif test-2channel.tif test-lzw.tif test-packbits.tif)) {
     push @p, $top->Photo(-file => $_);
     print "ok " . $ok++ . "\n";
+}
+
+if (!-d $tmp && !-w $tmp) {
+    for(; $ok <= $last; $ok++) {
+	print "ok $ok # skipping test: $tmp not writeable\n"
+    }
+    exit 0;
 }
 
 foreach (@p) {
     $top->Label(-image => $_)->pack(-side => 'left');
 }
-$p[0]->write('/tmp/tifftest2.tif');
+$top->update;
+sleep 1;
+$p[0]->write("$tmp/tifftest2.tif");
 
-print ((!-r '/tmp/tifftest2.tif' ? "not " : "") . "ok " . $ok++ . "\n");
+print ((!-r "$tmp/tifftest2.tif" ? "not " : "") . "ok " . $ok++ . "\n");
 
-$p2 = $top->Photo(-file => "/tmp/tifftest2.tif");
-$p2->write('/tmp/tifftest3.tif');
+$p2 = $top->Photo(-file => "$tmp/tifftest2.tif");
+$p2->write("$tmp/tifftest3.tif");
 
-print STDOUT ((compare("/tmp/tifftest2.tif", "/tmp/tifftest3.tif") != 0
+print STDOUT ((compare("$tmp/tifftest2.tif", "$tmp/tifftest3.tif") != 0
 	       ? "not " : "") . "ok " . $ok++ . "\n");
+
+$p2->write("$tmp/tifftest4.tif", '-format' => ['tiff', -compression => 'lzw']);
+print ((!-r "$tmp/tifftest4.tif" ? "not " : "") . "ok " . $ok++ . "\n");
+
+#cleanup
+for (<$tmp/tifftest*.tif>) {
+    unlink $_;
+}
 
 #MainLoop;
