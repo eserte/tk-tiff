ccc9a269818166b12a3995a682a7b190e09f1d88
Slaven Rezic <slaven@rezic.de>
1998-05-24 18:27:46 +0000

    0.02 use Tk 800.005

--- TIFF.pm
+++ TIFF.pm
@@ -1,13 +1,13 @@
 package Tk::TIFF;
 require DynaLoader;
-require Tk;
+use Tk 800.005;
 require Tk::Image;
 require Tk::Photo;
 
 use vars qw($VERSION @ISA);
 @ISA = qw(DynaLoader);
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 bootstrap Tk::TIFF $Tk::VERSION;
 
