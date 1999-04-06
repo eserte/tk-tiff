24f5b61dab5ca73be8dd18b323b3eca02b8b46a4
Slaven Rezic <slaven@rezic.de>
1999-04-06 22:40:22 +0000

    VERSION

--- TIFF.pm
+++ TIFF.pm
@@ -1,13 +1,13 @@
 package Tk::TIFF;
 require DynaLoader;
-use Tk 800.005;
+use Tk 800.014;
 require Tk::Image;
 require Tk::Photo;
 
 use vars qw($VERSION @ISA);
 @ISA = qw(DynaLoader);
 
-$VERSION = '0.03';
+$VERSION = '0.04';
 
 bootstrap Tk::TIFF $Tk::VERSION;
 
