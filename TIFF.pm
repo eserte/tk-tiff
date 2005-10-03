cb43f93a676f599749bc8d722142cebe2e1475b3
Slaven Rezic <slaven@rezic.de>
2005-10-03 21:07:14 +0000

    -> 0.10

--- TIFF.pm
+++ TIFF.pm
@@ -6,9 +6,18 @@ use Tk::Photo;
 use vars qw($VERSION @ISA);
 @ISA = qw(DynaLoader);
 
-$VERSION = '0.09';
+$VERSION = '0.10';
 
 bootstrap Tk::TIFF $Tk::VERSION;
+
+#
+# There are now two new functions (which may change!!!):
+#   # Get current value of global HistEqual Flag.
+#   $hist_equal = Tk::TIFF::getHistEqual();
+#
+#   # Set new value to true or false, returning new value.
+#   $hist_equal = Tk::TIFF::setHistEqual(0|1);
+#
 
 1;
 __END__
